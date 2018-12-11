11ebc111847ba25b727a7b7626a86d5313cb712e
rayjun <rayjun0412@gmail.com>
2018-12-11 15:15:53 +0800

    Update

--- leetcode/hashtable/Leetcode15.java
+++ leetcode/hashtable/Leetcode15.java
@@ -12,12 +12,84 @@ import java.util.Set;
  */
 public class Leetcode15 {
 
+
+    /**
+     * 这种也能实现，但是也还是超时
+     * @param nums
+     * @return
+     */
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> temp = null;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+
+        for (int i = 0; i < nums.length; i ++) {
+            if (map.get(nums[i]) > 1) {
+                map.put(nums[i], map.get(nums[i]) - 1);
+            } else {
+                map.remove(nums[i]);
+            }
+            for (int j = i+1; j < nums.length; j++) {
+                if (map.get(nums[j]) > 1) {
+                    map.put(nums[j], map.get(nums[j]) - 1);
+                } else {
+                    map.remove(nums[j]);
+                }
+
+                if(map.containsKey(-(nums[i]+ nums[j]))) {
+                    temp = new ArrayList<>();
+                    temp.add(nums[i]);
+                    temp.add(nums[j]);
+                    temp.add(-(nums[i] + nums[j]));
+                    boolean contain = false;
+
+                    for (List<Integer> l : result) {
+                        if(l.containsAll(temp) && temp.containsAll(l)) {
+                            contain = true;
+                        }
+                    }
+
+                      if(!contain) {
+                          result.add(temp);
+                      }
+                }
+
+                if (map.containsKey(nums[j])) {
+                    map.put(nums[j], map.get(nums[j]) + 1);
+                } else {
+                    map.put(nums[j], 1);
+                }
+                
+            }
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
+            }
+        }
+
+
+        return result;
+    }
+
+
     /**
      * 下面这种方法可以运行，但是超时
      * @param nums
      * @return
      */
-    public List<List<Integer>> threeSum(int[] nums) {
+    public List<List<Integer>> threeSum1(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
         List<Integer> temp = null;
 
