de579c18042a584d03ca2b05b614b672bb9aaaad
rayjun <rayjun0412@gmail.com>
2019-01-02 12:39:34 +0800

    Update

--- leetcode/array/Leetcode849.java
+++ leetcode/array/Leetcode849.java
@@ -6,17 +6,25 @@
 public class Leetcode849 {
 
     public int maxDistToClosest(int[] seats) {
-        int result = 0;
-        int cloeast = 0;
-        int before = 0;
-        int after = 0;        
-        for (int i = 0; i < seats.length; i++) {
-            if (seats[i] != 0) {
-                
+        int left = -1, maxDis = 0;
+        int len = seats.length;
+        
+        for (int i = 0; i < len; i++) {
+            if (seats[i] == 0) continue;
+
+            if (left == -1) {
+                maxDis = Math.max(maxDis, i);
+            } else {
+                maxDis = Math.max(maxDis, (i - left) / 2);
             }
-            
+            left = i;
         }
-        return result;
+        
+        if (seats[len - 1] == 0) {
+            maxDis = Math.max(maxDis, len - 1 - left);
+        }
+        
+        return maxDis;
     }
 
     
