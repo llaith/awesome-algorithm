cb8a697608d14ec0fa6578ad223b411d48a24b84
rayjun <rayjun0412@gmail.com>
2019-01-09 14:06:47 +0800

    Update

--- leetcode/linkedlist/Leetcode143.java
+++ leetcode/linkedlist/Leetcode143.java
@@ -5,6 +5,9 @@
 public class Leetcode143 {
 
     public void reorderList(ListNode head) {
+        if (head == null || head.next == null) {
+            return;
+        } 
         ListNode middle = null;
         ListNode p1 = null;
         middle = head;
@@ -14,15 +17,31 @@ public class Leetcode143 {
             p1 = p1.next.next;
         }
 
+        // 将链表的后半部分反转
         ListNode curNode = middle.next;
-        while(curNode.next != null) {
-            ListNode p2 = curNode.next;
+        ListNode p2 = curNode.next;
+        curNode.next = null;
+        while(p2 != null) {
             ListNode p3 = p2.next;
             p2.next = curNode;
             curNode = p2;
             p2 = p3;
         }
-        
+        middle.next = curNode;
+
+
+        // 将两部分的连接重新组合
+        ListNode p4 = head;
+        ListNode p5 = middle.next;
+        middle.next = null;
+        while(p4 != null && p5 != null) {
+            ListNode p6 = p5.next;
+            p5.next = p4.next;
+            p4.next = p5;
+            p5 = p6;
+            p4 = p4.next.next;
+        }
+
     }
     
     public static void main(String[] args) {
@@ -32,9 +51,14 @@ public class Leetcode143 {
         ListNode node1 = new ListNode(2);
         ListNode node2 = new ListNode(3);
         ListNode node3 = new ListNode(4);
+        ListNode node4 = new ListNode(5);
+        ListNode node5 = new ListNode(6);
         head.next = node1;
         node1.next = node2;
         node2.next = node3;
+        node3.next = node4;
+        node4.next = node5;
+        
 
         leetcode143.reorderList(head);
 
