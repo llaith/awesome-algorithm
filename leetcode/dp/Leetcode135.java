7cea053534636b84d1729d4f553f208f3d9f88cd
rayjun <rayjun0412@gmail.com>
2019-01-21 11:39:58 +0800

    Update

--- leetcode/dp/Leetcode135.java
+++ leetcode/dp/Leetcode135.java
@@ -5,8 +5,31 @@
 public class Leetcode135 {
 
     public int candy(int[] ratings) {
+        int[] arr1 = new int[ratings.length];
+        int[] arr2 = new int[ratings.length];
+        arr1[0] = 1;
+        arr2[ratings.length - 1] = 1;
+        for(int i = 1; i < ratings.length; i++) {
+            if (ratings[i - 1] >= ratings[i]) {
+                arr1[i] = 1;
+            } else {
+                arr1[i] = arr1[i - 1] + 1;
+            }
+        }
+        for(int i = ratings.length - 2; i >= 0; i--) {
+            if (ratings[i + 1] >= ratings[i]) {
+                arr2[i] = 1;
+            } else {
+                arr2[i] = arr2[i + 1] + 1;
+            }
+        }
+        int result = 0;
 
-        return 0;        
+        for (int i = 0; i < ratings.length; i++) {
+            result += Math.max(arr1[i], arr2[i]);
+        }
+        
+        return result;        
     }
 
     public static void main(String[] args) {
