a9e1a4b517e74924fca4a0fb449de9080344fc44
rayjun <rayjun0412@gmail.com>
2019-01-25 15:10:18 +0800

    Update

--- datastruct/graph/Graph.java
+++ datastruct/graph/Graph.java
@@ -3,16 +3,16 @@ import java.util.*;
 /** 
  * Graph
  */
-public class Graph {
+public class Graph { // 无向图
 
-    private int v;
-    private LinkedList<Integer> adj[];
+    private int v;  // 顶点的个数
+    private LinkedList<Integer> adj[]; // 邻接表
 
     public Graph(int v) {
         this.v = v;
         adj = new LinkedList[v];
         
-        for (int i = 0; i < v; i ++) {
+        for (int i = 0; i < v; i++) {
             adj[i] = new LinkedList<>();
         }
     }
@@ -20,6 +20,50 @@ public class Graph {
     public void addEdge(int s, int t) {
         adj[s].add(t);
         adj[t].add(s);
+    }
+
+
+    public void bfs(int s, int t) {
+        if (s == t) {
+            return;
+        }
+        boolean[] visited = new boolean[v];
+        visited[s] = true;
+
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(s);
+
+        int[] prev = new int[v];
+
+        for (int i = 0; i < v; i++) {
+            prev[i] = -1;
+        }
+
+        while (queue.size() != 0) {
+            int w = queue.poll();
+
+            for (int i = 0; i < adj[w].size(); i++) {
+                int q = adj[w].get(i);
+                if (!visited[q]) {
+                    prev[q] = w;
+                    if (q == t) {
+                        print(prev, s, t);
+                        return;
+                    }
+                    visited[q] = true;
+                    queue.add(q);
+                }
+            }
+        }
+
+    }
+
+
+    private void print(int[] prev, int s, int t) {
+        if (prev[t] != -1 && t != s) {
+            print(prev, s, prev[t]);
+        }
+        System.out.print(t+ "->");
     }
 
 }
