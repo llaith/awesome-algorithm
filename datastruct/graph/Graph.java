3a2060f05f2350a484916ab8d0b5922822c37b76
rayjun <rayjun0412@gmail.com>
2019-01-29 15:57:40 +0800

    Update

--- datastruct/graph/Graph.java
+++ datastruct/graph/Graph.java
@@ -27,30 +27,31 @@ public class Graph { // 无向图
         if (s == t) {
             return;
         }
-        boolean[] visited = new boolean[v];
+        boolean[] visited = new boolean[v]; //构建一个和图一样大小的数组，来记录访问过的节点
         visited[s] = true;
 
         Queue<Integer> queue = new LinkedList<>();
-        queue.add(s);
+        queue.add(s); //将当前访问过的相邻节点放入队列中 
 
-        int[] prev = new int[v];
+        int[] prev = new int[v];   // 记录路径的数组
 
         for (int i = 0; i < v; i++) {
             prev[i] = -1;
         }
 
         while (queue.size() != 0) {
-            int w = queue.poll();
+            int w = queue.poll(); // 取出一个还没有被访问过的节点
 
+            // 对该节点的相邻节点进行遍历
             for (int i = 0; i < adj[w].size(); i++) {
                 int q = adj[w].get(i);
-                if (!visited[q]) {
-                    prev[q] = w;
-                    if (q == t) {
+                if (!visited[q]) {  // 如果该节点还没有被访问
+                    prev[q] = w;   // 记录访问该节点是从哪个节点过来的
+                    if (q == t) {  // 找到终点，开始遍历路径
                         print(prev, s, t);
                         return;
                     }
-                    visited[q] = true;
+                    visited[q] = true;  // 将节点标记为已访问，入队列 
                     queue.add(q);
                 }
             }
